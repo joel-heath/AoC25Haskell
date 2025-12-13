67a9a4ee8f51f25834791ced8cced69ace71499e
Joel Heath <joelheath@outlook.com>
2025-12-13 19:52:33 +0000

    Day 12 done late OOPS

--- Day10.hs
+++ Day10.hs
@@ -1,7 +1,11 @@
 module Day10 (solve) where
 
-import Grid
 import Utils
+
+data Machine = Machine { lightDiagram :: [Bool],
+                         buttons :: [[Int]],
+                         requirements :: [Int],
+                         lights :: [Bool] } deriving (Show, Eq)
 
 solvePart1 :: [String] -> Int
 solvePart1 input =
@@ -18,4 +22,5 @@ solve input =
     let linesOfFile = lines input
         res1 = solvePart1 linesOfFile
         res2 = solvePart2 linesOfFile
-    in "Part 1: " ++ show res1 ++ "\nPart 2: " ++ show res2
+    -- in "Part 1: " ++ show res1 ++ "\nPart 2: " ++ show res2
+    in "Unsolved, it was hard enough in C#.\n(https://github.com/joel-heath/AdventOfCode2025/blob/master/AdventOfCode2025/Day10.cs)"
--- Day12.hs
+++ Day12.hs
@@ -1,21 +1,24 @@
+{-# OPTIONS_GHC -Wno-x-partial -Wno-unrecognised-warning-flags #-}
+{- HLINT ignore "Use map once" -} -- (it looks way more readable this way in my opinion)
+
 module Day12 (solve) where
 
 import Grid
 import Utils
+import Data.List.Split
 
-solvePart1 :: [String] -> Int
+solvePart1 :: String -> Int
 solvePart1 input =
-    let 
-    in undefined
-
-solvePart2 :: [String] -> Int
-solvePart2 input =
-    let 
-    in undefined
+    let regions = lines $ last $ splitOn "\n\n" input
+        stuff =
+            map (\r -> ((head (fst r) `div` 3) * (last (fst r) `div` 3),
+                                sum $ snd r)) .
+            map (\r -> (map read $ splitOn "x" $ head r,
+                        map read $ splitOn " " $ last r)) .
+            map (splitOn ": ") $ regions
+    in count (\r -> snd r <= fst r) stuff
 
 solve :: String -> String
-solve input = 
-    let linesOfFile = lines input
-        res1 = solvePart1 linesOfFile
-        res2 = solvePart2 linesOfFile
-    in "Part 1: " ++ show res1 ++ "\nPart 2: " ++ show res2
+solve input =
+    let res1 = solvePart1 input
+    in "Part 1: " ++ show res1
